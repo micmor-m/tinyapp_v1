3b8545da21baca41670d30b82281bf11ef6270c5
micmor-m <micmor.m@gmail.com>
2021-01-19 19:55:55 +0000

    add delete button to the show page and its route handler

--- express_server.js
+++ express_server.js
@@ -35,19 +35,19 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+
 app.post("/urls", (req, res) => {
-  //console.log(req.body); // Log the POST request body to the console
   //generete random short URL
   const newShortURL = generateRandomString();
   urlDatabase[newShortURL] = req.body.longURL;
-  console.log(urlDatabase);
-
-  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
   res.redirect(`/urls/${newShortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  //console.log(req.params);
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
